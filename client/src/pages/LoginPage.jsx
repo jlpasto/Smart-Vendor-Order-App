fdc6db502191a3e306ee16cab30ebcb43b00dc30
Jhon Loyd <jhonloydpastorin.03@gmail.com>
2026-01-30 02:17:33 +0800

    Changed the input type from "email" to "text" in LoginPage.jsx:124. The browser will no longer require an '@' symbol in the name field.

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -121,7 +121,7 @@ const LoginPage = () => {
                 </label>
                 <input
                   id="email"
-                  type="email"
+                  type="text"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   className="input"
